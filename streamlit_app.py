e692c2986d6d74fa5dc2b7fd44ce09edb48558c8
Rashmi PP <62315650+Rashmi-PP@users.noreply.github.com>
2023-07-06 17:55:16 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -49,7 +49,7 @@ if streamlit.button('Get fruit list'):
   my_data_rows = get_fruit_load_list()
   my_cnx.close()
   streamlit.dataframe(my_data_rows)
-*/
+
 streamlit.stop()
 
 # Add second text entry box
